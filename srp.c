33f59ed2877adc3f0f1517a04e309ff3dda3cbd4
walter083 <115606559+walter083@users.noreply.github.com>
2024-11-09 13:38:48 +0530

    Update srp.c

--- srp.c
+++ srp.c
@@ -1,72 +1,71 @@
 #include<stdio.h>
 #include<string.h>
-struct prodRule {
-	char left[30],right[30];
+
+struct prodRule{
+	char left[20],right[20];
 };
 
 int main(){
-	int rn,i,j,k,stktop;
-	char temp[50],*t1,*t2,input[50],ch[2],stack[10],*matchstr;
+	int np,i;
+	struct prodRule rules[20];
+	char temp[30],*t1,*t2;
+	char input[10],stack[20],ch[2],*match;
+	int stacktop;
+	
+	//input productions
+	printf("enter no of productions:");
+	scanf("%d",&np);
+	printf("enter productions:");
+	for(i=0;i<np;i++){
+		scanf("%s",temp);
+		t1=strtok(temp,"->");
+		t2=strtok(NULL,"->");
+		strcpy(rules[i].left,t1);
+		strcpy(rules[i].right,t2);	
+	}
+	//input the string
+	printf("enter the strings:");
+	scanf("%s",input);
 	
 	stack[0]='\0';
 	
-	printf("Enter the no of production rules:");
-	scanf("%d",&rn);
-	struct prodRule rules[rn];
-	printf("Enter the rules:\n");
-	for(i=0;i<rn;i++){
-		scanf("%s",temp);
-		t1=strtok(temp,"->");
-		strcpy(rules[i].left,t1);
-		t2=strtok(NULL,"->");
-		strcpy(rules[i].right,t2);
-	}
-	/*printf("printing rules..\n");
-	for(i=0;i<rn;i++){
-		printf("%s->%s\n",rules[i].left,rules[i].right);
-	}*/
-	
-	printf("enter input:");
-	scanf("%s",input);
-	printf("string is %s\n",input);
+	printf("Stack\tInput\tAction\n");
 	i=0;
 	while(1){
-	
-		if(i<strlen(input)){
-			ch[0]=input[i];
-			ch[1]='\0';
-			i++;
-			strcat(stack,ch);
-			printf("%s\t",stack);
-			
-			for(k=i;k<strlen(input);k++){
-				printf("%c",input[k]);	
-			}
-			printf("\tShift %s\n",ch);
+		//Shift
+		ch[0]=input[i];
+		ch[1]='\0';
+		strcat(stack,ch);
+		printf("%s\t",stack);
+		
+		for(int k=i;k<strlen(input);k++){
+			printf("%c",input[k]);
 		}
-		for(j=0;j<rn;j++){
-			matchstr=strstr(stack,rules[j].right);
-			if(matchstr!=NULL){
-				stktop=strlen(stack)-strlen(matchstr);
-				stack[stktop]='\0';
-				
+		printf("\tShift %s\n",ch);
+		//Reduce
+		for(int j=0;j<np;j++){
+			match=strstr(stack,rules[j].right);
+			if(match!=NULL){
+				stacktop=strlen(stack)-strlen(match);
+				stack[stacktop]='\0';
 				strcat(stack,rules[j].left);
-				
 				printf("%s\t",stack);
-				for(k=i;k<strlen(input);k++){
-				printf("%c",input[k]);	
+				for(int k=i;k<strlen(input);k++){
+					printf("%c",input[k]);
 				}
-			printf("\tReduce %s->%s\n",rules[j].left,rules[j].right);
+				printf("\tReduce %s->%s\n",rules[j].left,rules[j].right);
 			}
 		}
-		if (i == strlen(input) && strcmp(stack, rules[0].left) == 0) {
-        printf("Accepted\n");
-        break;
-    } else if (i == strlen(input)) {
-        printf("Not accepted\n");
-        break;
-    }
+		i++;
+		if(strlen(input)==i){
+			break;
+		}
 	}
-	
+	if(strcmp(stack,rules[0].left)==0){
+		printf("string accepted\n");
+	}
+	else{
+		printf("string is not accepted\n");
+	}
 	return 0;
 }
